ee363d127e15d42b869e27c323140a66e4c773e6
Dmitry B.V <83430886+prog420@users.noreply.github.com>
2023-11-30 19:42:31 +0500

    [*] fix: rename test_03_* test names

--- tests/test_03_notifications.py
+++ tests/test_03_notifications.py
@@ -6,7 +6,7 @@ from src.pages import NotificationPage
 @allure.feature("Notifications")
 class TestAddRemoveElements(BaseCase):
     @allure.title("TC 03.001 User Is Able Open Notifications Page")
-    def test_02_001_able_to_open_page(self):
+    def test_03_001_able_to_open_page(self):
         notif_page = NotificationPage(self.driver)
         notif_page.open()
         assert notif_page.is_opened()
@@ -15,7 +15,7 @@ class TestAddRemoveElements(BaseCase):
         "TC 03.002 Success notification is displayed "
         "after click on 'Click here' button"
     )
-    def test_02_002_able_to_add_remove_elements(self):
+    def test_03_002_success_notif_is_displayed_after_click_here(self):
         notif_page = NotificationPage(self.driver)
         notif_page.open()
         notif_page.show_notification()
