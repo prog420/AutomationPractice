5a9b37d6642454a0f869a4ce3c22744b409c4092
Dmitry B.V <83430886+prog420@users.noreply.github.com>
2023-11-30 19:40:18 +0500

    [+] Add function to format locators

--- src/base/base_locators.py
+++ src/base/base_locators.py
@@ -1,2 +1,19 @@
+from typing import Tuple
+
+
 class BasePageLocators:
-    ...
+    @staticmethod
+    def format_locator(locator: Tuple[str, str], *args, **kwargs) \
+            -> Tuple[str, str]:
+        """
+        Format **selector** part of the locator.
+
+            Example:
+        `format((By.XPATH, '//td[parent::tr/td/text()="{}"]'), 'Chrome')`
+
+            Output:
+        `(By.XPATH, '//td[parent::tr/td/text()="Chrome"]')`
+        """
+        by, value = locator
+        formatted_value = value.format(*args, **kwargs)
+        return by, formatted_value
