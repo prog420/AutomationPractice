58b66203857a5c0627a6f6657549117e3cccfa8f
Dmitry B.V <83430886+prog420@users.noreply.github.com>
2023-11-30 23:47:59 +0500

    [*] Change config data container from dict to dataclass

--- conftest.py
+++ conftest.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Union
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
@@ -5,9 +9,28 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+class PageLoadStrategy(Enum):
+    """
+    More info about page load strategies:
+    https://www.lambdatest.com/blog/selenium-page-load-strategy/
+    https://www.selenium.dev/documentation/webdriver/drivers/options/
+    """
+    NONE = "none"
+    NORMAL = "normal"
+    EAGER = "eager"
+
+
+@dataclass
+class Config:
+    headless: bool = None
+    browser: str = None
+    cookie_enabled: bool = None
+    options: Union[ChromeOptions] = None
+
+
 def pytest_addoption(parser: pytest.Parser):
     parser.addoption("--headless", action="store_true")
-    parser.addoption("--no-cookie", action="store_true")
+    parser.addoption("--no-cookies", action="store_true")
     parser.addoption("--browser", default="chrome")
 
 
@@ -19,52 +42,47 @@ def config(request: pytest.FixtureRequest):
     https://petertc.medium.com/pro-tips-for-selenium-setup-1855a11f88f8
     https://www.google.com/googlebooks/chrome/med_26.html
     """
-    headless: bool = request.config.getoption("--headless")
-    browser: str = request.config.getoption("--browser")
-    no_cookie: bool = request.config.getoption("--no-cookie")
+    config = Config()
+    config.headless = request.config.getoption("--headless")
+    config.browser = request.config.getoption("--browser")
+    config.cookie_enabled = not request.config.getoption("--no-cookies")
 
-    options = None
-    if browser.lower() == "chrome":
+    if config.browser.lower() == "chrome":
         options = ChromeOptions()
         # Install ad block extension
         options.add_extension(
             "external_files/extensions/uBlock0_1.52.0.chromium.zip"
         )
-        if no_cookie:
+        if not config.cookie_enabled:
             options.add_experimental_option(
                 "prefs",
                 {"profile.default_content_setting_values.cookies": 2}
             )
     else:
-        raise ValueError(f"Browser {browser} is not supported.")
+        raise ValueError(f"Browser {config.browser} is not supported.")
 
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    options.set_capability("pageLoadStrategy", "normal")
-    # options.set_capability("pageLoadStrategy", "eager")
+    options.set_capability("pageLoadStrategy", PageLoadStrategy.NORMAL.value)
 
-    if headless:
+    if config.headless:
         options.add_argument("-headless")
 
-    return {
-        "browser": browser, "headless": headless, "options": options,
-        "no_cookie": no_cookie
-    }
+    config.options = options
+    return config
 
 
 @pytest.fixture(scope="function")
 def driver(config):
     driver = None
-    browser = config.get("browser")
-    options = config.get("options")
     executable_path = ChromeDriverManager().install()
     # executable_path = "external_files/drivers/chromedriver_119.exe"
 
-    if browser == "chrome":
+    if config.browser == "chrome":
         driver = webdriver.Chrome(
-            options=options, service=ChromeService(executable_path)
+            options=config.options, service=ChromeService(executable_path)
         )
     yield driver
     driver.quit()
--- tests/test_05_browser_info.py
+++ tests/test_05_browser_info.py
@@ -7,7 +7,7 @@ from src.pages import BrowserInfoPage
 @allure.feature("Browser Information")
 class TestDynamicTable(BaseCase):
     @allure.title("TC 05.001 User Is Able Open Browser Information Page")
-    def test_04_001_able_to_open_page(self):
+    def test_05_001_able_to_open_page(self):
         info_page = BrowserInfoPage(self.driver)
         info_page.open()
         assert info_page.is_opened()
@@ -15,7 +15,7 @@ class TestDynamicTable(BaseCase):
     @allure.title(
         "TC 05.002 User is able to see browser information"
     )
-    def test_04_002_chrome_cpu_table_value_is_equal_to_output_value(self):
+    def test_05_002_chrome_cpu_table_value_is_equal_to_output_value(self):
         info_page = BrowserInfoPage(self.driver)
         info_page.open()
         info_page.toggle_browser_info()
@@ -32,16 +32,15 @@ class TestDynamicTable(BaseCase):
                          "equal to browser's user agent"):
             assert info_page.get_user_agent() == info_page.text_user_agent
 
-    @pytest.mark.wip
     @allure.title(
         "TC 05.004 'Cookies enabled' value is equal to browser settings value"
     )
-    def test_05_003_cookie_enabled_is_equal_to_browser_settings(self):
+    def test_05_004_cookie_enabled_is_equal_to_browser_settings(self):
         info_page = BrowserInfoPage(self.driver)
         info_page.open()
         info_page.toggle_browser_info()
         with allure.step(
                 "Check if 'Cookie Enabled' value is equal to value from "
                 "browser settings"):
-            assert str(self.config["no_cookie"]).lower() != \
+            assert str(self.config.cookie_enabled).lower() == \
                    info_page.text_cookie
